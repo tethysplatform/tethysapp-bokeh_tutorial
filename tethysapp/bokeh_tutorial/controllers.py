dfbe6ec7749bcd6d16019208921c62b3e4815177
nswain <nswain@aquaveo.com>
2019-11-09 17:19:51 -0700

    Update for Tethys Platform 3.0 Release

--- tethysapp/bokeh_tutorial/controllers.py
+++ tethysapp/bokeh_tutorial/controllers.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
-from tethys_sdk.permissions import login_required
-
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, Slider
-from bokeh.layouts import column
-from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
 from bokeh.embed import server_document
-
-import param
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.plotting import figure
+from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
+from django.shortcuts import render
 import panel as pn
-import numpy as np
+from tethys_sdk.permissions import login_required
+from tethys_sdk.gizmos import Button
+from .param_model import ShapeViewer
+
+
+@login_required()
+def home(request):
+    script = server_document(request.build_absolute_uri())
+    context = {'script': script}
+    return render(request, 'bokeh_tutorial/home.html', context)
 
 
 def home_handler(document):
@@ -33,80 +38,14 @@ def home_handler(document):
 
     document.add_root(column(slider, plot))
 
-
 @login_required()
-def home(request):
+def shapes_with_panel(request):
     script = server_document(request.build_absolute_uri())
     context = {'script': script}
-    return render(request, 'bokeh_tutorial/home.html', context)
-
-
-class Shape(param.Parameterized):
-    radius = param.Number(default=1, bounds=(0, 1))
-
-    def __init__(self, **params):
-        super(Shape, self).__init__(**params)
-        self.figure = figure(x_range=(-1, 1), y_range=(-1, 1), width=500, height=500)
-        self.renderer = self.figure.line(*self._get_coords())
-
-    def _get_coords(self):
-        return [], []
-
-    def view(self):
-        return self.figure
-
-
-class Circle(Shape):
-    n = param.Integer(default=100, precedence=-1)
-
-    def __init__(self, **params):
-        super(Circle, self).__init__(**params)
-
-    def _get_coords(self):
-        angles = np.linspace(0, 2 * np.pi, self.n + 1)
-        return (self.radius * np.sin(angles),
-                self.radius * np.cos(angles))
-
-    @param.depends('radius', watch=True)
-    def update(self):
-        xs, ys = self._get_coords()
-        self.renderer.data_source.data.update({'x': xs, 'y': ys})
-
-
-class NGon(Circle):
-    n = param.Integer(default=3, bounds=(3, 10), precedence=1)
-
-    @param.depends('radius', 'n', watch=True)
-    def update(self):
-        xs, ys = self._get_coords()
-        self.renderer.data_source.data.update({'x': xs, 'y': ys})
-
-
-shapes = [NGon(name='NGon'), Circle(name='Circle')]
-
-
-class ShapeViewer(param.Parameterized):
-    shape = param.ObjectSelector(default=shapes[0], objects=shapes)
-
-    @param.depends('shape')
-    def view(self):
-        return self.shape.view()
-
-    @param.depends('shape', 'shape.radius')
-    def title(self):
-        return '## %s (radius=%.1f)' % (type(self.shape).__name__, self.shape.radius)
-
-    def panel(self):
-        return pn.Column(self.title, self.view)
+    return render(request, "bokeh_tutorial/shapes.html", context)
 
 
 def shapes_handler(document):
     viewer = ShapeViewer()
     panel = pn.Row(viewer.param, viewer.panel())
     panel.server_doc(document)
-
-
-def shapes_with_panel(request):
-    script = server_document(request.build_absolute_uri())
-    context = {'script': script}
-    return render(request, "bokeh_tutorial/shapes.html", context)
